698434788d24e43e3129792da9a7d7ae4a638914
goli-harika <harika.ists01@gmail.com>
2025-07-02 13:00:48 +0530

    Add files via upload

---
+++ name of the month and days.py
@@ -0,0 +1,27 @@
+mon_num=int(input("enter the month number 1 to 12"))
+if mon_num==1:
+    print("the month is january it has 31 days..")
+elif mon_num==2:
+     print("the month is feb it has 28 days..")
+elif mon_num==3:
+     print("the month is ar it has 31 days..")
+elif mon_num==4:
+       print("the month is april it has 30 days..")
+elif mon_num==5:
+      print("the month is may it has 31 days..")
+elif mon_num==6:
+     print("the month is june it has 30 days..")
+elif mon_num==7:
+     print("the month is july it has 31 days..")
+elif mon_num==8:
+     print("the month is aug it has 31 days..")
+elif mon_num==9:
+     print("the month is sep it has 30 days..")
+elif mon_num==10:
+     print("the month is oct it has 31 days..")
+elif mon_num==11:
+     print("the month is nove it has 30 days..")
+elif mon_num==12:
+     print("the month is dec it has 31 days..")
+else:
+     print("wrong number")
