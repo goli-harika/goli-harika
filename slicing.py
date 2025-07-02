c0082ffee962630c537952b382811509248e2523
goli-harika <harika.ists01@gmail.com>
2025-07-02 19:12:01 +0530

    Add files via upload

---
+++ slicing.py
@@ -0,0 +1,10 @@
+"""
+slicing :is used to retrive the elements from one particular range to another particular range 
+syntax:s*3 (start:stop:skip)
+"""
+mylist=[1,2,3,4,5,6,7,8,9]
+print(mylist)
+print(mylist[0:6:2])
+mylist1=[2,3,5,6,8,9,10]
+print(mylist1)
+print(mylist1[0:7:3])
